8fe692d65067ab9eb7024a5150d68751988ac2e4
yoon <yoon@172.31.57.194>
2026-04-02 14:43:45 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+    	//test
         System.out.println("Hello world!2222");
         System.out.println("Hello world!22334455");
     }
