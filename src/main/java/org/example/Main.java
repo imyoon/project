3db61707e287e26e8b63ee20cc9ffecf95211ff4
yoon <yoon@172.31.57.194>
2026-04-02 14:44:16 +0900

    2222

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+    	//test localbranch
+    	//ddd
         System.out.println("Hello world!2222");
         System.out.println("Hello world!22334455");
     }
